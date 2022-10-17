3add6bd45b885e5d97b3dec954fc81b39a03ffc5
wssiripong <w.ssiripong1@gmail.com>
2022-10-17 15:54:12 +0700

    feat:config route

---
+++ src/route/Router.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import Register from '../features/Register';
+
+function Router() {
+  return (
+    <Routes>
+      <Route path='/' />
+    </Routes>
+  );
+}
+
+export default Router;
