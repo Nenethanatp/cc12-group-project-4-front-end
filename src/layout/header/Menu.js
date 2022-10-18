0271b4a6152baccf321ecdd6d068b73a677d16ff
rachataphongl <rachataphong.hi@gmail.com>
2022-10-18 21:35:19 +0700

    feat: add animation by p'phu

--- src/layout/header/Menu.js
+++ src/layout/header/Menu.js
@@ -1,16 +1,17 @@
 function Menu({ handleMenu }) {
   return (
-    <div className="flex  bg-black bg-opacity-40 fixed z-50  top-0 right-0 bottom-0 left-0">
-      <div className="flex fixed flex-col justify-between  text-black h-full w-full  pt-14 px-7 pb-7  bg-white rounded-3xl left-14">
+    <div
+      onClick={handleMenu}
+      className="flex  bg-black bg-opacity-40 fixed z-50  top-0 right-0 bottom-0 left-0 justify-end "
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="flex flex-col justify-between  text-black h-full w-[80%]  pt-14 px-7 pb-7  bg-white rounded-tl-3xl rounded-bl-3xl faderightside"
+      >
         <div>
-          <div className="flex items-center gap-3 text-xl font-bold border-b-2 border-gray-400 pb-3   mb-2 ">
+          <div className="flex items-center gap-3 text-xl font-bold border-b-2 border-gray-400 pb-3 mb-2 ">
             <div className="w-[50px] h-[50px] bg-slate-300 rounded-[40px] "></div>
             <div>WEB</div>
-
-            <button className="flex absolute right-24" onClick={handleMenu}>
-              <span class="material-symbols-outlined">close</span>
-            </button>
-            {/** */}
           </div>
           <div>
             <div className="mb-3">Home</div>
@@ -21,7 +22,7 @@ function Menu({ handleMenu }) {
         </div>
         <div>
           <div className="flex items-center gap-3 text-xl font-bold border-t-2 border-gray-400 pt-3  mb-2">
-            <div className="w-[50px] h-[50px] bg-slate-300 rounded-[40px] "></div>
+            <div className="w-[50px] w- h-[50px] bg-slate-300 rounded-[40px] "></div>
             <div>John Doe</div>
           </div>
           <div className="mb-3">Go to profile</div>
