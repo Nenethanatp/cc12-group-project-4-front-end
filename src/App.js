4de1119ab4c431d5aea9dac0d713a599909847ec
wssiripong <w.ssiripong1@gmail.com>
2022-10-18 15:41:59 +0700

    feat:getMe done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import Router from './route/Router';
 import { ToastContainer } from 'react-toastify';
+import { getMe } from './store/authSlice';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import * as authService from './api/authApi';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getUser = () => async (dispatch) => {
+      const res = await authService.getMe();
+      dispatch(getMe(res.data.user));
+    };
+    dispatch(getUser());
+  }, [dispatch]);
+
   return (
     <>
       <Router />
