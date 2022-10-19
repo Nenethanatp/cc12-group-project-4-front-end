b24ba709cd1baa579570fce3c32b8857a17594ba
wssiripong <w.ssiripong1@gmail.com>
2022-10-19 14:56:24 +0700

    feat:googleLogin

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -5,3 +5,5 @@ export const register = (input) => axios.post('/auth/register', input);
 export const login = (input) => axios.post('/auth/login', input);
 
 export const getMe = () => axios.get('/auth/me');
+
+export const loginGoogle = (input) => axios.post('/auth/googleLogin', input);
--- src/features/auth/LoginForm.js
+++ src/features/auth/LoginForm.js
@@ -1,6 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from '../../store/authSlice';
+import { gapi } from 'gapi-script';
+import LoginGoogle from '../../components/LoginGoogle';
+
+const clientId =
+  '713136136398-r4nrmvg52fnsad1f466mnnq48ldh1862.apps.googleusercontent.com';
 
 function LoginForm() {
   const [input, setInput] = useState({
@@ -9,6 +14,16 @@ function LoginForm() {
   });
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ''
+      });
+    }
+    gapi.load('client:auth2', start);
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,22 +39,25 @@ function LoginForm() {
       <div className='flex flex-col items-center gap-5'>
         <div className='text-2xl font-bold m-5'>SIGN IN</div>
         <input
-          className='bg-gray-200 h-11 w-[320px] px-5'
+          className='bg-gray-200 h-11 w-[320px] px-5 shadow-md'
           placeholder='Email'
           value={input.email}
           onChange={(e) => setInput({ ...input, email: e.target.value })}
         />
         <input
           type='password'
-          className='bg-gray-200 h-11 w-[320px] px-5'
+          className='bg-gray-200 h-11 w-[320px] px-5 shadow-md'
           placeholder='Password'
           value={input.password}
           onChange={(e) => setInput({ ...input, password: e.target.value })}
         />
-        <div className='basis-2/5 flex items-center'>
-          <button className='bg-yellow-400 w-[320px] h-12 rounded-full font-bold text-lg'>
+        <div className='basis-2/5 flex flex-col justify-center gap-3'>
+          <button className='bg-yellow-400 w-[320px] h-12 rounded-full font-bold text-lg shadow-md'>
             LOG IN
           </button>
+          <div className='flex justify-center'>
+            <LoginGoogle />
+          </div>
         </div>
       </div>
     </form>
