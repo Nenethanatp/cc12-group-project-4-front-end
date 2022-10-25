9f2d8593d486d0b62b335f62c8e5df35290a6df7
rachataphongl <rachataphong.hi@gmail.com>
2022-10-26 00:45:00 +0700

    fix feat:follow3

--- src/layout/profile/ProfileNav.js
+++ src/layout/profile/ProfileNav.js
@@ -1,10 +1,8 @@
-import FollowList from './FollowList';
-
-function ProfileNav({ allFollower, handleClickFollow, handlePost }) {
+function ProfileNav({ handleClickFollow, openPost }) {
   return (
     <>
       <div className="h-12 flex gap-10 bg-gray-200 py-3 pl-10 rounded-t-3xl">
-        <button onClick={() => handlePost()}>Post</button>
+        <button onClick={() => openPost()}>Post</button>
         <button onClick={() => handleClickFollow()}>FollowList</button>
       </div>
     </>
