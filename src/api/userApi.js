0215b079424d74a5e0fed64e2832fed7ea251ad2
KaewSathita <kaew.gothai@gmail.com>
2022-10-27 19:11:01 +0700

    feat addFavorite

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -3,3 +3,6 @@ import axios from '../config/axios';
 export const getUserByName = (input) => axios.get(`/user/find?name=${input}`);
 
 export const updateUserApi = (input) => axios.patch(`/user/me`, input);
+
+export const addFavorite = (input) => axios.post('/user/add-favorite', input);
+export const getFavorites = () => axios.get('/user/favorites');
