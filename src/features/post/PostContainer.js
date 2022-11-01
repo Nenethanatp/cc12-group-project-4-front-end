379dd467ed8ed4a92f513151b18f7ec56ab25cc8
Thanatporn Lawsirirattana <nthalaw21@gmail.com>
2022-11-01 14:05:54 +0700

    feat: add subscribe open modal in post container

--- src/features/post/PostContainer.js
+++ src/features/post/PostContainer.js
@@ -1,17 +1,20 @@
-import { useState } from "react";
-import PostForm from "./PostForm";
-import AddFavoriteForm from "./AddFavoriteForm";
-import PostList from "./PostList";
-import { useDispatch } from "react-redux";
-import { createPost } from "../../store/postSlice";
-import { createFavorite } from "../../store/favoriteSlice";
-import Modal from "../../components/Modal";
+import { useState } from 'react';
+import PostForm from './PostForm';
+import AddFavoriteForm from './AddFavoriteForm';
+import PostList from './PostList';
+import { useDispatch, useSelector } from 'react-redux';
+import { createPost } from '../../store/postSlice';
+import { createFavorite } from '../../store/favoriteSlice';
+import Modal from '../../components/Modal';
+import ModalSubscribe from '../favorite/ModalSubscribe';
 
 function PostContainer() {
   const [isCreatePostOpen, setIsCreatePostOpen] = useState(false);
   const [isAddFavoriteOpen, setIsAddFavoriteOpen] = useState(false);
+  const [subscribeModalOpen, setSubscribeModalOpen] = useState(false);
 
   const dispatch = useDispatch();
+  const status = useSelector((state) => state.auth.status);
 
   const toggleCreatePost = () => {
     setIsCreatePostOpen((prev) => !prev);
@@ -21,15 +24,23 @@ function PostContainer() {
     setIsAddFavoriteOpen((prev) => !prev);
   };
 
+  const handleClickFav = () => {
+    if (status === 'subscribed') {
+      toggleAddFavorite();
+    } else {
+      setSubscribeModalOpen(true);
+    }
+  };
+
   const handleCreatePost = (input) => {
     const formData = new FormData();
-    formData.append("content", input.content);
-    formData.append("typeId", input.typeId);
-    formData.append("userId", input.userId);
-    formData.append("latitude", input.latitude);
-    formData.append("longitude", input.longitude);
+    formData.append('content', input.content);
+    formData.append('typeId', input.typeId);
+    formData.append('userId', input.userId);
+    formData.append('latitude', input.latitude);
+    formData.append('longitude', input.longitude);
     for (let i = 0; i < input.postImages.length; i++) {
-      formData.append("postImage", input.postImages[i]);
+      formData.append('postImage', input.postImages[i]);
     }
     // console.log(formData);
     dispatch(createPost(formData));
@@ -42,13 +53,11 @@ function PostContainer() {
     toggleAddFavorite();
   };
 
-
   return (
     <>
       <div className="h-full">
         <div className="bg-slate-200 h-full rounded-t-3xl p-6 relative">
           <div className="grid grid-cols-2 gap-4">
-
             <div>
               <button
                 className="bg-amber-400 rounded-3xl p-3 text-lg font-semibold w-full"
@@ -60,7 +69,7 @@ function PostContainer() {
             <div>
               <button
                 className="bg-slate-50 rounded-3xl p-3 text-lg font-semibold w-full"
-                onClick={toggleAddFavorite}
+                onClick={handleClickFav}
               >
                 ADD FAVORITE
               </button>
@@ -68,7 +77,6 @@ function PostContainer() {
           </div>
 
           <PostList />
-
         </div>
 
         <Modal
@@ -91,6 +99,19 @@ function PostContainer() {
             />
           }
           close={toggleCreatePost}
+        />
+        <Modal
+          open={subscribeModalOpen}
+          content={
+            <ModalSubscribe
+              closeModal={() => {
+                setSubscribeModalOpen(false);
+              }}
+            />
+          }
+          close={() => {
+            setSubscribeModalOpen(false);
+          }}
         />
       </div>
     </>
