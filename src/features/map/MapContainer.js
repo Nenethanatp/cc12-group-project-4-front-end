f8b280a865a3a1d2052e502e712d253726d8e23a
BestSTN <bestflashcs3@hotmail.com>
2022-11-02 12:17:25 +0700

    refactor code map favorite fixed click on marker

--- src/features/map/MapContainer.js
+++ src/features/map/MapContainer.js
@@ -1,40 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Category from "../../components/Category";
 import Modal from "../../components/Modal";
 import PostContainer from "../post/PostContainer";
 import FavoriteContainer from "../favorite/FavoriteContainer";
 import Map from "./Map";
-import { toast } from "react-toastify";
 import ModalSubscribe from "../favorite/ModalSubscribe";
 
 function MapContainer() {
   const [openPost, setOpenPost] = useState(false);
   const [openFavorite, setOpenFavorite] = useState(false);
   const [openSubscribe, setOpenSubscribe] = useState(false);
-  const [mapCenter, setMapCenter] = useState({ lat: 13.75, lng: 100.5 });
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setMapCenter({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        });
-      },
-      () => toast.error("can not get your location")
-    );
-  }, []);
 
   const handleOpenPost = () => {
     setOpenPost(true);
   };
 
-  const handleSetMapCenter = (input) => {
-    // console.log(input);
-    setMapCenter({
-      lat: +input.latitude,
-      lng: +input.longitude,
-    });
+  const handleCloseFavorite = () => {
+    setOpenFavorite(false);
   };
 
   return (
@@ -45,7 +27,7 @@ function MapContainer() {
           setOpenSubscribe(true);
         }}
       />
-      <Map handleOpenPost={handleOpenPost} mapCenter={mapCenter} />
+      <Map handleOpenPost={handleOpenPost} />
       <Modal
         open={openPost}
         content={<PostContainer />}
@@ -53,8 +35,10 @@ function MapContainer() {
       ></Modal>
       <Modal
         open={openFavorite}
-        content={<FavoriteContainer handleSetMapCenter={handleSetMapCenter} />}
-        close={() => setOpenFavorite(false)}
+        content={
+          <FavoriteContainer handleCloseFavorite={handleCloseFavorite} />
+        }
+        close={handleCloseFavorite}
       ></Modal>
 
       <Modal
